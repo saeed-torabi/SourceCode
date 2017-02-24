9c061d4c5ab68f1f78bb94939c2359e7aaee58df
Ali <Ali@DESKTOP-E73NJUV>
2017-02-24 08:19:30 +0330

    Add three of the table scripts into table folder physically

---
+++ Board/Board.DB/Tables/Ads.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Ads](
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[CreatedBy] [int] NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[Title] [nvarchar](100) NOT NULL,
+	[Description] [nvarchar](max) NOT NULL,
+	[CategoryId] [int] NOT NULL,
+	[Approved] [bit] NOT NULL,
+	[UpdateDate] [datetime] NULL,
+ CONSTRAINT [PK_Ads_1] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
